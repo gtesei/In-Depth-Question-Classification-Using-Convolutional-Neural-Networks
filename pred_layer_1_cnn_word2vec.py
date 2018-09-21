3e296b644a5499cd0149bc0213e9837d937f3882
Tesei <gtesei@deloitte.com>
2018-09-21 16:28:13 -0700

    ...

--- pred_layer_1_cnn_word2vec.py
+++ pred_layer_1_cnn_word2vec.py
@@ -13,7 +13,6 @@ np.random.seed(2)
 maxim = 32
 w2v = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)  
 
-
 def categorical_probas_to_classes(p):
     return np.argmax(p, axis=1)
 
@@ -40,32 +39,44 @@ def acquire_data(filename, model, maxim, index):
     loc_targets = [index] * len(filename)
     return loc_data, np.array(loc_targets)
 
+
+def classify_question(test_text):
+    ## 
+    data = []
+    targets = []
+    loc_data, loc_targets = acquire_data(test_text, w2v, maxim, -1)
+    print loc_data.shape, loc_targets.shape           
+    data.append(loc_data)
+    data = np.array(data)
+    data = np.vstack(data)
+    x_test = data
+    x_test = np.reshape(x_test, (x_test.shape[0], maxim, 300, 1))
+    model = load_model('word2vec_main_model_10.h5')
+    predictions = model.predict(x_test)
+    prediction_labels = categorical_probas_to_classes(predictions)
+    prediction_labels = prediction_labels.tolist()
+    ## ['abbr.txt', 'desc.txt', 'enty.txt', 'hum.txt', 'loc.txt', 'num.txt']
+    lab_dict = {0: "Abbreviation" , 1: "Description" , 2: "Entity" , 3: "Human" , 4: "Location" , 5: "Numeric"}
+    prediction_labels = [lab_dict[i] for i in prediction_labels]
+    print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", "Prediction:" , prediction_labels
+
 ###
 #test_text = [' What is the full form of .com ? ', ' What does the abbreviation AIDS stand for ? ', " What does INRI stand for when used on Jesus ' cross ? "]
 
-test_text = [str(sys.argv[1])]
-print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", test_text , " is ..."
+if __name__ == '__main__':
+    #test_text = [str(sys.argv[1])]
+    print ">>>>>>>>>>>>> Type EXIT to exit <<<<<<<<<<<<<<<<<"
+    for line in sys.stdin:
+        test_text = line.replace("\n","")
+        if line == "EXIT":
+            print("bye...")
+            break
+        else:
+            print ">>>>>>>>>>>>>", test_text , " is ..."
+            classify_question(test_text)
 
-## 
-data = []
-targets = []
-loc_data, loc_targets = acquire_data(test_text, w2v, maxim, -1)
-print loc_data.shape, loc_targets.shape           
-data.append(loc_data)
-data = np.array(data)
-data = np.vstack(data)
-x_test = data
-x_test = np.reshape(x_test, (x_test.shape[0], maxim, 300, 1))
-model = load_model('word2vec_main_model_10.h5')
-predictions = model.predict(x_test)
-prediction_labels = categorical_probas_to_classes(predictions)
-prediction_labels = prediction_labels.tolist()
 
-## ['abbr.txt', 'desc.txt', 'enty.txt', 'hum.txt', 'loc.txt', 'num.txt']
-lab_dict = {0: "Abbreviation" , 1: "Description" , 2: "Entity" , 3: "Human" , 4: "Location" , 5: "Numeric"}
 
-prediction_labels = [lab_dict[i] for i in prediction_labels]
 
-print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", "Prediction:" , prediction_labels
 
 
